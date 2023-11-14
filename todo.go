826d2963d581057f8d48a709b87622d2e3b95555
Kudzeri <kudzeri@gmail.com>
2023-11-14 19:17:40 +0600

    add: Complete func

--- todo.go
+++ todo.go
@@ -1,1 +1,38 @@
-package cmd
+package CLI_Todo_App
+
+import (
+	"errors"
+	"time"
+)
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type Todos []item
+
+func (t *Todos) Add(task string) {
+	todo := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*t = append(*t, todo)
+}
+
+func (t *Todos) Complete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("INVALID INDEX")
+	}
+
+	ls[index-1].CreatedAt = time.Now()
+	ls[index-1].Done = true
+
+	return nil
+}
