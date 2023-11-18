4cf2015ccd5f9aac5954cc4e5a8305cae158d0cf
Kudzeri <kudzeri@gmail.com>
2023-11-18 17:39:22 +0600

    add:'add flag' and its implementation, auto-load file

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	todo "github.com/kudzeri/todo-app"
+	"os"
+)
+
+const (
+	todoFile          = ".todos.json"
+	ErrInvalidCommand = "INVALID COMMAND"
+)
 
 func main() {
-	fmt.Println("Hello word!")
+
+	add := flag.Bool("add", false, "add a new todo")
+	//complete := flag.Bool("complete", false, "complete a todo")
+	//delete := flag.Bool("delete", false, "delete a todo")
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Println(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Println(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, ErrInvalidCommand)
+		os.Exit(1)
+	}
 }
